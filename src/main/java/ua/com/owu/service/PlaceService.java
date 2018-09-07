5772e3d963dcd6fc06167f70fd54bbc22d12c2f2
TarasFiliuk <filiuk1803@gmail.com>
2018-09-07 16:25:13 +0300

    sadf

---
+++ src/main/java/ua/com/owu/service/PlaceService.java
@@ -0,0 +1,11 @@
+package ua.com.owu.service;
+
+import ua.com.owu.models.Event;
+import ua.com.owu.models.Place;
+
+public interface PlaceService  {
+    Place findByAdress (String adress);
+    Place findByCity (String city);
+    Place findByName (String name);
+    Place findByEvents (Event event);
+}
---
+++ src/main/java/ua/com/owu/service/PlaceServiceImp.java
@@ -0,0 +1,33 @@
+package ua.com.owu.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ua.com.owu.dao.PlaceDAO;
+import ua.com.owu.models.Event;
+import ua.com.owu.models.Place;
+@Service
+@Transactional
+public class PlaceServiceImp implements PlaceService {
+    @Autowired
+    PlaceDAO placeDAO;
+    @Override
+    public Place findByAdress(String adress) {
+        return placeDAO.findByAdress(adress);
+    }
+
+    @Override
+    public Place findByCity(String city) {
+        return placeDAO.findByCity(city);
+    }
+
+    @Override
+    public Place findByName(String name) {
+        return placeDAO.findByName(name);
+    }
+
+    @Override
+    public Place findByEvents(Event event) {
+        return placeDAO.findByEvents(event);
+    }
+}
