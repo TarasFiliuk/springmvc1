0dce6eacf014f59faf7da9ee0b3b30da24a5a512
TarasFiliuk <filiuk1803@gmail.com>
2018-09-07 16:38:04 +0300

    sadf

--- src/main/java/ua/com/owu/service/managerService/ManagerService.java
+++ src/main/java/ua/com/owu/service/managerService/ManagerService.java
@@ -1,8 +1,8 @@
 package ua.com.owu.service.managerService;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import ua.com.owu.models.Manager;
 
-public interface ManagerService {
-    Manager findByUsername (String username);
+public interface ManagerService extends UserDetailsService {
     Manager findByFirstName(String firstname);
 }
--- src/main/java/ua/com/owu/service/managerService/ManagerServiceImp.java
+++ src/main/java/ua/com/owu/service/managerService/ManagerServiceImp.java
@@ -1,6 +1,8 @@
 package ua.com.owu.service.managerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ua.com.owu.dao.ManagerDAO;
 import ua.com.owu.models.Manager;
@@ -10,13 +12,14 @@ import ua.com.owu.service.managerService.ManagerService;
 public class ManagerServiceImp implements ManagerService {
     @Autowired
     ManagerDAO managerDAO;
-    @Override
-    public Manager findByUsername(String username) {
-        return managerDAO.findByUsername(username);
-    }
 
     @Override
     public Manager findByFirstName(String firstname) {
         return managerDAO.findByFirstName(firstname);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return managerDAO.findByUsername(username);
     }
 }
