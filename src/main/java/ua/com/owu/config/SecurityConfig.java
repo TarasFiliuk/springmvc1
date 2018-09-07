72b7e988737701d4c063193ef745e189960d19f3
tania.kliuieva <tania.kliuieva@gmail.com>
2018-09-07 17:22:24 +0300

    fixing

--- src/main/java/ua/com/owu/config/SecurityConfig.java
+++ src/main/java/ua/com/owu/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ua.com.owu.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    @Qualifier("userServiceImpl")
     UserDetailsService userDetailsService;
 
     @Bean
