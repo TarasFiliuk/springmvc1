5e1fdce06d337684ee9a071984311491173653aa
tania.kliuieva <tania.kliuieva@gmail.com>
2018-10-02 15:57:20 +0300

    Dao and Services Update

--- src/main/java/ua/com/owu/controller/EventRestController.java
+++ src/main/java/ua/com/owu/controller/EventRestController.java
@@ -1,4 +1,32 @@
 package ua.com.owu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.com.owu.models.Event;
+import ua.com.owu.service.eventService.EventService;
+
+@RestController
+@RequestMapping("events")
 public class EventRestController {
+
+    private final EventService eventService;
+
+    @Autowired
+    public EventRestController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+            )
+    public Event createEvent(@RequestBody Event event){
+        return eventService.save(event);
+    }
+
 }
--- src/main/java/ua/com/owu/dao/EventDAO.java
+++ src/main/java/ua/com/owu/dao/EventDAO.java
@@ -1,9 +1,16 @@
 package ua.com.owu.dao;
 
+import org.joda.time.DateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.com.owu.models.Event;
+import ua.com.owu.models.Place;
+
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface EventDAO extends JpaRepository<Event,Integer> {
-    Event findByEventId (int id);
+    List<Event> findByPlace(Place place);
+    List<Event> findByDate(DateTime dateTime);
 }
